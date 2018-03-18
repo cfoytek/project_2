0ca06038a79000c238af61d54335d0d7434cd43f
Chris Foytek <cff15@txstate.edu>
2018-03-18 18:51:52 -0500

    Implement backspace

--- src/proj2.c
+++ src/proj2.c
@@ -16,24 +16,24 @@ int main(){
 	int ch;
 	enum modes {NORMAL = 0, INSERT = 1}; //0 = Normal, 1 = Insert
 	WINDOW *e_win; //Editor window
-	int x = 1;
+	int x = 0;
   int y = 1;
 	int maxx, maxy, xbound, ybound;
 
-	initscr();			/* Start curses mode 		*/
+	initscr();			/* Start curses mode */
 	//cbreak();			/* Enable character at a time buffering */
-	raw();				/* Line buffering disabled	*/
-	keypad(stdscr, TRUE);		/* We get F1, F2 etc..		*/
+	raw();				/* Line buffering disabled */
+	keypad(stdscr, TRUE);		/* We get F1, F2 etc.. */
 	noecho();			/* Don't echo() while we do getch */
 
   printw("Press ESC to quit\n");
 	refresh();
 
 	getmaxyx(stdscr, maxy, maxx);
-	//Set boundaries for window
+	//Set boundaries for editor window
 	xbound = maxx - 5;
 	ybound = maxy - 5;
-	e_win = create_newwin(ybound, xbound - 5, y, x);
+	e_win = create_newwin(ybound, xbound, y, x);
 	scrollok(e_win, TRUE);
 	wmove(e_win, y, x);
 	wrefresh(e_win);
@@ -41,42 +41,62 @@ int main(){
 	getbegyx(e_win, y, x);
 	print_coords(e_win, x, y);
 	wmove(e_win, y, x);
+  wrefresh(e_win);
 	while((ch = getch()) != 27) { //27 is char code for ESC or ALT
-			//Using ESC to exit will cause a delay because ncurses will
-			//Wait to see if the character is ALT and wait for another 
-			//keypress before accepting that the key is ESC
-			print_coords(e_win, x, y); //Print current window coordinates
-			refresh();
-			switch (ch){
-				case KEY_LEFT:
-					if(x > 0)
-						x--;
-					break;
-				case KEY_RIGHT:
-					if(x < xbound)
-						x++;
-					break;
-				case KEY_UP:
-					if(y > 0)
-						y--;
-					break;
-				case KEY_DOWN:
-					if(y < ybound)
-						y++;
-					break;
-				case KEY_ENTER:
-					break;
-				case 127: //Backspace char code
-				case KEY_DC:
-				case KEY_BACKSPACE:
-					delch();
-					wrefresh(e_win);
-					break;
-				default:
-					waddch(e_win, (char)ch);
-					x++;
-					wrefresh(e_win);
+    //Using ESC to exit will cause a delay because ncurses will
+    //Wait to see if the character is ALT and wait for another 
+    //keypress before accepting that the key is ESC
+    switch (ch) {
+      case KEY_LEFT:
+        if(x > 0)
+          x--;
+        else
+          beep();
+        break;
+      case KEY_RIGHT:
+        if(x < xbound)
+          x++;
+        else
+          beep();
+        break;
+      case KEY_UP:
+        if(y > 0)
+          y--;
+        else
+          beep();
+        break;
+      case KEY_DOWN:
+        if(y < ybound)
+          y++;
+        else
+          beep();
+        break;
+      case KEY_ENTER:
+        wprintw(e_win, "\n");
+        break;
+      case 127: //Backspace char code
+      case KEY_DC:
+      case KEY_BACKSPACE:
+        if(x > 0)
+          x--;
+        else if(y > 0) {
+          x = xbound;
+          y--;
+        }
+        else
+          beep();
+        mvwdelch(e_win, y, x);
+        break;
+      default:
+        waddch(e_win, (char)ch);
+        if(x < xbound)
+          x++;
+        else {
+          x = 1;
+          y++;
+        }
 		}
+    print_coords(e_win, x, y); //Print current window coordinates
 		wmove(e_win, y, x);
 		wrefresh(e_win);   
   }
@@ -89,8 +109,13 @@ int main(){
  * right corner of the stdscr
  **/
 void print_coords(WINDOW * win, int x, int y) {
+  //TODO: Create another window called "Status Window"
+  //      and print these coordinates to it, as well as
+  //      input commands to it i.e. how you write and quit
+  //      in Vim
 	int maxy, maxx;
 	getmaxyx(win, maxy, maxx);
-	move(maxy, maxx - 10);
+	move(maxy + 1, maxx - 7);
 	printw("(%d, %d)", x, y);
+  refresh();
 }
