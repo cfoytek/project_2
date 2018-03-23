1ce3cd9d335459290bf6441629a4d0f6201dc04b
Chris Foytek <cff15@txstate.edu>
2018-03-23 10:33:55 -0500

    Implement reading/writing file to/from file buffer (untested)

--- include/file_handler.h
+++ include/file_handler.h
@@ -8,15 +8,29 @@
 #ifndef _FILE_HANDLER_H
 #define _FILE_HANDLER_H
 #include <stdio.h>
+#include <errno.h>
+#include <stdbool.h>
 
-/** File Buffer **/
+const int M_LN_SIZE = 156;
 char **file_buf;
+extern int errno;
 /**
  * Fill buffer with contents of file.
  **/
-void read_file(const char *file_name);
+void read_file(const char *file_name, int *line_count);
 /**
  * Write buffer's contents to file.
  **/
 void write_file(const char *file_name);
+/**
+ * Free all memory associated with the file
+ * buffer.
+ **/
+void destory_buf();
+/**
+ * This overridden destory_buf will first write
+ * the file buffer to the file file_name before
+ * destorying the buffer.
+ **/
+void destroy_buf(const char *file_name);
 #endif
---
+++ src/file_handler.c
@@ -0,0 +1,67 @@
+#include <file_handler.h>
+
+void read_file(const char *file_name, int *line_count) {
+    file_buf = NULL;
+    FILE *fp = NULL;
+    int i;
+    int line_counter = 0;
+    int line_len;
+    int line[M_LN_SIZE];    //Adds restriction that lines must be less than M_LN_SIZE
+    *line_count = 0;        //Zero out return value
+    fp = fopen(file_name, "r");
+    if(fp == NULL) {
+        fprintf(stderr, "Cannot read file.\n%s", strerror(errno));
+        exit(1);
+    }
+    //Count number of lines in the file
+    while(fgets(line, sizeof(line), fp) != NULL) {
+        line_counter++;
+    }
+    rewind(fp);
+    //Allocate space for lines in buffer
+    file_buf = malloc(line_counter * sizeof(char *));
+    if(file_buf == NULL) {
+       exit(1);            //Error allocating memory
+    }
+    //Copy lines from file into buffer
+    for(i = 0; i < line_counter; i++) {
+        fgets(line, sizeof(line), fp);  //We will preserve '\n'
+        line_len = strlen(line);
+        file_buf[i] = malloc((line_len + 1) * sizeof(char));
+        strcpy(file_buf[i], line);
+    }
+    *line_count = line_counter;
+}
+
+void write_file(const char *file_name) {
+    FILE *fp = NULL;
+    int line_count;
+    fp = fopen(file_name, "w");
+    if(fp == NULL) {
+        fprintf(stderr, "Cannot open file for writing.\n%s", strerror(errno));
+        exit(1);
+    }
+    line_count = sizeof(file_buf) / sizeof(file_buf[0]);
+    int i;
+    while(i < line_count) {
+        const char *line = file_buf[i];
+        fputs(line, fp);
+        i++;
+    }
+    fclose(fp);
+}
+
+void destory_buf() {
+    int line_count = sizeof(file_buf) / sizeof(file_buf[0]);
+    int i;
+    for(i = 0; i < line_count; i++) {
+        free(file_buf[i]);
+    }
+    free(file_buf);
+    file_buf = NULL;
+}
+
+void destory_buf(const char *file_name) {
+    write_file(file_name);
+    destory_buf();
+}
